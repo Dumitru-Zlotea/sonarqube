94ddc8008c28893a058f027cac481c90444f9f06
Dima <zlotea_dumitru@yahoo.com>
2025-02-07 21:33:43 -0500

    added new feature

--- sonar-scanner-engine/src/test/java/org/sonar/scanner/new_functionality/NewFunctionality.java
+++ sonar-scanner-engine/src/test/java/org/sonar/scanner/new_functionality/NewFunctionality.java
@@ -7,59 +7,59 @@ import static org.assertj.core.api.Assertions.*;
 import org.sonar.scanner.report.StringManipulation;
 
 class StringManipulationMediumIT {
-    public static final String stringA = "Hello ";
-    public static final String stringB = "there";
+  public static final String STRING_A = "Hello ";
+  public static final String STRING_B = "there";
 
-    private StringManipulation sm;
+  private StringManipulation sm;
 
-    @BeforeEach
-    void setUp() {
-	sm = new StringManipulation(stringA, stringB);
-    }
+  @BeforeEach
+  void setUp() {
+	  sm = new StringManipulation(STRING_A, STRING_B);
+  }
 
-    @Test
-    void test_getter() {
-	var a = sm.getStringA();
-	var b = sm.getStringB();
-	assertThat(a).isEqualTo(stringA);
-	assertThat(b).isEqualTo(stringB);
-    }
+  @Test
+  void testGetter() {
+	  var a = sm.getStringA();
+	  var b = sm.getStringB();
+	  assertThat(a).isEqualTo(STRING_A);
+	  assertThat(b).isEqualTo(STRING_B);
+  }
 
-    @Test
-    void test_setter() {
-	var newA = "newA";
-	var newB = "newB";
-	sm.setStringA(newA);
-	sm.setStringB(newB);
-	assertThat(sm.getStringA()).isEqualTo(newA);
-	assertThat(sm.getStringB()).isEqualTo(newB);
-    }
+  @Test
+  void testSetter() {
+	  var newA = "newA";
+	  var newB = "newB";
+	  sm.setStringA(newA);
+	  sm.setStringB(newB);
+	  assertThat(sm.getStringA()).isEqualTo(newA);
+	  assertThat(sm.getStringB()).isEqualTo(newB);
+  }
 
-    @Test
-    void test_concatenate() {
-	var concat = sm.concatenate();
-	assertThat(concat).isEqualTo(stringA + stringB);
-    }
+  @Test
+  void testConcatenate() {
+	  var concat = sm.concatenate();
+	  assertThat(concat).isEqualTo(STRING_A + STRING_B);
+  }
 
-    @Test
-    void test_is_equal_false() {
-	assertThat(sm.isEqual()).isFalse();
-    }
+  @Test
+  void testIsEqualFalse() {
+	  assertThat(sm.isEqual()).isFalse();
+  }
 
-    @Test
-    void test_is_equal_true() {
-	var equal_sm = new StringManipulation(stringA, stringA);
-	assertThat(equal_sm.isEqual()).isTrue();
-    }
+  @Test
+  void testIsEqualTrue() {
+	  var equalSm = new StringManipulation(STRING_A, STRING_A);
+	  assertThat(equalSm.isEqual()).isTrue();
+  }
 
-    @Test
-    void test_contains_false() {
-	assertThat(sm.contains()).isFalse();
-    }
+  @Test
+  void testContainsFalse() {
+	  assertThat(sm.contains()).isFalse();
+  }
 
-    @Test
-    void test_contains_true() {
-	var contains_sm = new StringManipulation(stringA, "Hello");
-	assertThat(contains_sm.contains()).isTrue();
-    }
+  @Test
+  void testContainsTrue() {
+	  var containsSm = new StringManipulation(STRING_A, "Hello");
+	  assertThat(containsSm.contains()).isTrue();
+  }
 }
